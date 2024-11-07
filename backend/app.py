466095b093104038d05ceac5ba45a47faf5a7b8e
Lander Van der Stighelen <lander@vanderstighelen.net>
2024-11-07 14:04:30 +0000

    Refactor app.py to remove Redis initialization and use configuration for access token expiration

--- backend/app.py
+++ backend/app.py
@@ -3,16 +3,14 @@ from quart import Quart
 from quart_cors import cors
 from quart_jwt_extended import JWTManager
 import os
+from config import ACCESS_EXPIRES, revoked_store
 from dotenv import load_dotenv
-from datetime import timedelta
-import redis
 
 load_dotenv()
 
 app = Quart(__name__)
 app = cors(app)
 
-ACCESS_EXPIRES = timedelta(days=365)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'my_precious')
 app.config['JWT_SECRET_KEY'] = app.config['SECRET_KEY']
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = ACCESS_EXPIRES
@@ -21,21 +19,15 @@ app.config["JWT_BLACKLIST_TOKEN_CHECKS"] = ["access"]
 
 jwt = JWTManager(app)
 
-revoked_store = redis.StrictRedis(
-    host="KL-redis", port=6379, db=0, decode_responses=True
-)
-
 from api import auth_blueprint, api_blueprint
 app.register_blueprint(auth_blueprint)
 app.register_blueprint(api_blueprint)
 
-# Check if a token is blacklisted
 @jwt.token_in_blacklist_loader
 def check_if_token_is_revoked(decrypted_token):
     jti = decrypted_token['jti']
     return revoked_store.get(jti) is not None
 
-# Run startup tasks
 @app.before_serving
 async def startup():
     from api.auth import insert_admin_user
