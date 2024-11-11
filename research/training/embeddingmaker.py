d4df231fb61056f9e90105a35c41a085db73e561
RedPandaMC <jurreandenys@gmail.com>
2024-11-11 16:16:32 +0100

    updated file structure

---
+++ research/training/embeddingmaker.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import torch
+from sentence_transformers import SentenceTransformer
+
+model_path = "../../backend/checket/finetuned_paraphrase-multilingual-MiniLM-L12-v2/final"
+model = SentenceTransformer(model_path)
+
+training_data_path = "cleaned_training.csv"
+training_data = pd.read_csv(training_data_path)
+
+sampled_training_data = training_data.sample(frac=0.1, random_state=42)
+training_sentences = sampled_training_data['sentence1'].tolist()
+
+training_embeddings = model.encode(training_sentences, convert_to_tensor=True)
+
+torch.save(training_embeddings, "training_embeddings.pt")
