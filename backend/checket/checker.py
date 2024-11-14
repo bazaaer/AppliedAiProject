ce3567bcefb22676934b29395462096197499889
RedPandaMC <jurreandenys@gmail.com>
2024-11-14 14:11:51 +0100

    edited checker so it doesn't shadow var names from outer scope

--- backend/checket/checker.py
+++ backend/checket/checker.py
@@ -2,29 +2,31 @@ import torch
 from sentence_transformers import SentenceTransformer
 
 class SimilarityEvaluator:
-    def __init__(self, training_embeddings_path, device=None):
+    def __init__(self, base_embeddings_path, device=None):
         self.device = (
             device if device else ("cuda" if torch.cuda.is_available() else "cpu")
         )
+
         self.model = SentenceTransformer(
             "ODeNy/Checket_Antwerpen_Huisstijl_MiniLM", device=device
         )
-        self.training_embeddings = torch.load(
-            training_embeddings_path, weights_only=True
+
+        self.base_embeddings = torch.load(
+            base_embeddings_path, weights_only=True
         ).to(self.device)
 
-    def topk_mean_similarity_score(self, new_sentence, k=5):
-        new_embedding = self.model.encode(
-            new_sentence, convert_to_tensor=True, device=self.device
+    def topk_mean_similarity_score(self, sentence, k=5):
+        embedding = self.model.encode(
+            sentence, convert_to_tensor=True, device=self.device
         )
 
         similarities = self.model.similarity(
-            new_embedding.unsqueeze(0), self.training_embeddings
+            embedding.unsqueeze(0), self.base_embeddings
         )
 
         mean_topk_score = torch.topk(similarities, k).values.mean().item()
 
-        return new_sentence, mean_topk_score
+        return mean_topk_score
 
 
 # Usage
@@ -33,8 +35,8 @@ if __name__ == "__main__":
 
     similarity_evaluator = SimilarityEvaluator(training_embeddings_path)
 
-    sentence, score = similarity_evaluator.topk_mean_similarity_score(
-        """De Britse realityster en presentatrice <strong>Narinder Kaur</strong> (51) ligt zwaar onder vuur na haar uithaal naar prinses Kate op "X"."""
-    )
+    sentence = """Op 16 november meert Sinterklaas met de boot aan in Antwerpen. Kom jij Sinterklaas en zijn kleurrijk gevolg begroeten langs de route?"""
 
-    print(f"Sentence: {sentence}\nTop-K Mean Similarity Score: {score:.8f}")
+    score = similarity_evaluator.topk_mean_similarity_score(sentence,k=8)
+
+    print(f"Sentence: {sentence}\nTop-K Mean Similarity Score: {score:.3f}")
