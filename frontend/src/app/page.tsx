a4b2cefbce6df92022203ee4469d88429e999c95
SolidSalad <grimmieboy17@gmail.com>
2024-11-21 11:11:52 +0100

    deleted unused components

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,16 +3,12 @@ import { Navbar, Footer } from "@/components";
 
 // sections
 import Hero from "./hero";
-import Posts from "./posts";
-import Articles from "./articles";
 
 export default function Campaign() {
   return (
     <>
       <Navbar />
       <Hero />
-      <Posts />
-      <Articles />
       <Footer />
     </>
   );
