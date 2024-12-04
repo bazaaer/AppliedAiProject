5267758d744914add95302f93c78b08f53165741
Lander Van der Stighelen <lander@vanderstighelen.net>
2024-12-04 23:45:38 +0000

    Improve sayserve pipeline performance

--- backend/checket/rayserve.py
+++ backend/checket/rayserve.py
@@ -2,6 +2,7 @@ from ray import serve
 from starlette.requests import Request
 from grouper import SentenceGrouper
 from checker import SimilarityEvaluator
+from ray.serve.handle import DeploymentHandle
 
 
 @serve.deployment(ray_actor_options={"num_gpus": 0.2})
@@ -16,11 +17,12 @@ class SentenceGrouperDeployment:
     async def process(self, text: str):
         return self.grouper.group_consecutive_similar_sentences(text)
 
-    async def __call__(self, request: Request):
-        request_data = await request.json()
+    async def __call__(self, request: dict):
+        request_data = request
+
         text = request_data.get("text", "")
-        result = await self.process(text)
-        return {"result": result}
+        result = [sentence[0] for sentence in await self.process(text)]
+        return {"sentences": result}
 
 
 @serve.deployment(ray_actor_options={"num_gpus": 0.8})
@@ -39,13 +41,39 @@ class SimilarityEvaluatorDeployment:
             for sentence in sentences
         ]
 
-    async def __call__(self, request: Request):
-        request_data = await request.json()
+    async def __call__(self, request: dict):
+        request_data = request
+
         sentences = request_data.get("sentences", [])
         result = await self.process(sentences)
-        return {"result": result}
+        return {"sentence_scores": result}
 
 
-# Bind deployments into a pipeline
+@serve.deployment
+class Pipeline:
+    """
+    A pipeline that chains SentenceGrouperDeployment and SimilarityEvaluatorDeployment.
+    """
+
+    def __init__(self, grouper: DeploymentHandle, evaluator: DeploymentHandle):
+        self.grouper = grouper
+        self.evaluator = evaluator
+
+    async def process(self, text: str):
+        grouper_result = await self.grouper.remote({"text": text})
+
+        evaluator_result = await self.evaluator.remote(grouper_result)
+
+        return evaluator_result
+
+    async def __call__(self, request: Request):
+        request_data = await request.json()
+        text = request_data.get("text", "")
+        result = await self.process(text)
+        return result
+
+
+# Bind the deployments together
 grouper_app = SentenceGrouperDeployment.bind(model="nl_core_news_md", similarity_threshold=0.80)
 evaluator_app = SimilarityEvaluatorDeployment.bind(training_embeddings_path="embeddings.pt", top_k=8)
+pipeline_app = Pipeline.bind(grouper_app, evaluator_app)
