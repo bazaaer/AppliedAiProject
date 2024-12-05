494b39ebb681de80c78a001bd3805778b5cc9b1c
jVaerewijck <vaerewijckjarn@gmail.com>
2024-12-05 10:54:13 +0100

    api request works

--- frontend/ckeditor5_build/src/plugins/testPlugin.js
+++ frontend/ckeditor5_build/src/plugins/testPlugin.js
@@ -29,36 +29,40 @@ export default class testPlugin extends Plugin {
         const editor = this.editor;
 
         let previewText = editor.getData();
-        this._sendTextToApi(previewText).then(apiResponse => {
-            previewText = apiResponse;
-        });
+        previewText = this._sendTextToApi(previewText)
         console.log(previewText)
 
         editor.setData(previewText);
         const Div = document.createElement('div');
         Div.innerHTML = '<p>Original text score: ???</p><p>New text score: ???</p>';
         Div.style.position = 'absolute';
-        Div.style.bottom = '10vh';
+        Div.style.top = '10vh';
         Div.style.right = '-10vw';
         document.body.appendChild(Div);
     }
     //haal verbeterde tekst van api op
     async _sendTextToApi(text) { // https://klopta.vinnievirtuoso.online/api/docs/index.html https://klopta.vinnievirtuoso.online/api/rewrite
-        const response = await fetch('https://klopta.vinnievirtuoso.online/api/rewrite', {
+        const requestData = {
+            text: text
+          };
+        const response = await fetch('https://klopta.vinnievirtuoso.online/api/texts/rewrite', {
             method: 'POST',
             headers: {
+                'Accept': 'application/json',
+                'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MzMzODk3MDMsIm5iZiI6MTczMzM4OTcwMywianRpIjoiNmU0OGM5OTUtMDJjYS00YmRhLWI1YmYtMGNhOTA5MzA1N2YwIiwiZXhwIjoxNzMzOTk0NTAzLCJpZGVudGl0eSI6ImFkbWluIiwiZnJlc2giOmZhbHNlLCJ0eXBlIjoiYWNjZXNzIiwidXNlcl9jbGFpbXMiOnsicm9sZSI6ImFkbWluIn19.HzBSQ5EsyOPJoTVxEC8ik0do1X2_O9XSDEJ_7XX9K_I',
                 'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ text })
-        });
 
-        // if (!response.ok) {
-        //     throw new Error('API request failed');
-        // }
+            },
+            body: JSON.stringify(requestData)
+        }).catch(error => {
+            console.error('Error:', error);
+          });
 
         const result = await response.json();
         //tijdelijke aanpassing
-        const newtext = result
+        const newtext = result.data.text
+        console.log(result.msg)
+        console.log(`new text: ${newtext}`)
         return newtext;
     }
 }
