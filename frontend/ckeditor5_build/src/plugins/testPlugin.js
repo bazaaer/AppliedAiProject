83c602dfcdfd2dc95359c245843257f3033c1532
jVaerewijck <vaerewijckjarn@gmail.com>
2024-12-19 09:53:57 +0100

    did the thing

--- frontend/ckeditor5_build/src/plugins/testPlugin.js
+++ frontend/ckeditor5_build/src/plugins/testPlugin.js
@@ -33,13 +33,33 @@ export default class testPlugin extends Plugin {
         const editorElement = this.editor.ui.view.element;
 
         let previewText = editor.getData();
+        console.log(`Current text:${previewText}`)
         let text = ""
         text = await this._sendTextToApi(previewText,apiKey)
         console.log(text)
 
+        let sentences = "";
+        let originalscore = 0;
+        sentences = await this._scoreApi(previewText,apiKey)
+        let i = 0
+        sentences.forEach(({ score, sentence }) => {
+            originalscore += score
+            i += 1
+        })
+        originalscore = Math.round((originalscore/i)*100)/100
+
+        let newscore = 0;
+        sentences = await this._scoreApi(text,apiKey)
+        i = 0
+        sentences.forEach(({ score, sentence }) => {
+            newscore += score
+            i += 1
+        })
+        newscore = Math.round((newscore/i)*100)/100
+
         editor.setData(`${text}`);
         const Div = document.createElement('div');
-        Div.innerHTML = '<p style="margin: 0;">Original text score: ???</p><p style="margin: 0;">New text score: ???</p>';
+        Div.innerHTML = `<p style="margin: 0;">Originele score: ${originalscore}</p><p style="margin: 0;">Nieuwe score: ${newscore}</p>`;
         Div.style.position = 'absolute';
         Div.style.top = '5px';
         Div.style.right = '5px';
@@ -69,9 +89,31 @@ export default class testPlugin extends Plugin {
         const result = await response.json();
         console.log(result)
         //tijdelijke aanpassing
-        const newtext = result.data.text
+        const newtext = result.data
         console.log(result.msg)
         console.log(`new text: ${newtext}`)
-        return `${newtext}<p>${result.msg}<p/>`;
+        return `${newtext}`;
+    }
+    async _scoreApi(text,apiKey) {
+        const requestData = {
+            text: text
+          };
+        const response = await fetch('https://klopta.vinnievirtuoso.online/api/model/score', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization':`Bearer ${apiKey}`,
+                'Content-Type': 'application/json'
+
+            },
+            body: JSON.stringify(requestData)
+        }).catch(error => {
+            console.error('Error:', error);
+          });
+        const result = await response.json();
+        console.log(result)
+        //tijdelijke aanpassing
+        const sentences = result.sentence_scores
+        return sentences;
     }
 }
