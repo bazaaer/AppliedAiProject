6a179acd4854e55e2638f75035da2f4d671d804f
Lander Van der Stighelen <lander@vanderstighelen.net>
2025-01-08 18:26:58 +0000

    Refactor Dockerfile and Nginx configuration for static export and improved routing

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Enables static export
+  trailingSlash: true, // Adds a trailing slash for better compatibility with static hosting
   images: {
     remotePatterns: [
       {
@@ -7,6 +9,7 @@ const nextConfig = {
         hostname: "**",
       },
     ],
+    unoptimized: true, // Disables image optimization for static export
   },
   typescript: {
     ignoreBuildErrors: true, // Ignores TypeScript errors during the build process
