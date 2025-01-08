153e342eea6703175d1df69ffd324b977856a893
Lander Van der Stighelen <lander@vanderstighelen.net>
2025-01-08 17:49:33 +0000

    Fix build errors by dynamically importing the components

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-// components
-import { Navbar, Footer } from "@/components";
 import React from "react";
+import dynamic from "next/dynamic";
 
-// sections
-import Demo from "../components/demo";
-import Body from "../components/body";
+// Dynamically import components to ensure SSR safety
+const Navbar = dynamic(() => import("@/components").then((mod) => mod.Navbar), { ssr: false });
+const Footer = dynamic(() => import("@/components").then((mod) => mod.Footer), { ssr: false });
+const Demo = dynamic(() => import("../components/demo"), { ssr: false });
+const Body = dynamic(() => import("../components/body"), { ssr: false });
 
 // context
 import { AuthProvider } from "@/context/authContext";
